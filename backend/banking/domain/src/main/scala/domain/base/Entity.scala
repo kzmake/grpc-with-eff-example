8db5097c42efdf960a86e7593ad5fefb776c95c6
kzmake <kzmake.i3a@gmail.com>
2022-03-18 21:49:03 +0900

    refactor

--- backend/banking/domain/src/main/scala/domain/base/Entity.scala
+++ backend/banking/domain/src/main/scala/domain/base/Entity.scala
@@ -6,8 +6,4 @@ trait Entity[T <: Entity[_]] {
   def id: Id[T]
 
   override def hashCode: Int = id.hashCode
-  override def equals(that: Any): Boolean = that match {
-    case x: Entity[T] => this.id.eq(x.id)
-    case _            => false
-  }
 }
